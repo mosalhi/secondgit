38acae504cd203a109f0f685efed68c7e5e2ab2b
mosalhi <mosalhi@gmail.com>
2016-01-31 17:37:11 +0300

    toto-po

--- app/myscript/common.js
+++ app/myscript/common.js
@@ -5,7 +5,7 @@ $(function() {
 		$("img[src*='svg']").attr("src", function() {
 			return $(this).attr("src").replace(".svg", ".png");
 		});
-	}
+	} 
 
 	//E-mail Ajax Send
 	//Documentation & Example: https://github.com/agragregra/uniMail
@@ -24,7 +24,6 @@ $(function() {
 		});
 		return false;
 	});
-
 	//Chrome Smooth Scroll
 	try {
 		$.browserSelector();
